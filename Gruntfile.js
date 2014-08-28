82c37f304a5ff4ddc15189d33b0ae1b46e1e6360
wankyu <wankyu@nhn.com>
2014-08-28 17:32:15 +0900

    npm package 생성시 latest버전 생성하는 task 추가

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@
 
 module.exports = function(grunt) {
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
     uit_index: {
       index: {
         options: {
@@ -23,13 +24,29 @@ module.exports = function(grunt) {
           download : true
         }
       }
+    },
+    shell: {
+      dist: {
+        command: 'npm pack'
+      }
+    },
+    copy: {
+      dist: {
+        src: 'grunt-nts-uit-index-<%= pkg.version %>.tgz',
+        dest: 'dist/grunt-nts-uit-index.latest.tgz'
+      }
     }
   });
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
   // By default, lint and run all tests.
   grunt.registerTask('default', ['uit_index']);
+  grunt.registerTask('dist', ['shell:dist', 'copy:dist']);
 
 };
